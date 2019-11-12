46ca332b12352f32a60844a98fc43459f543d0c2
Ma. Rowena Tan <rowena.tan0712@gmail.com>
2019-11-12 15:24:33 +0800

    3des first commit

---
+++ src/main/java/com/encryption/tripledes/TripledesApplication.java
@@ -0,0 +1,13 @@
+package com.encryption.tripledes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripledesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripledesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/encryption/tripledes/TripledesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encryption.tripledes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripledesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
