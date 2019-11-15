992e4d819d14d44138a7e0651b1d1e58cc1d81f5
Ma. Rowena Tan <rowena.tan0712@gmail.com>
2019-11-15 10:57:24 +0800

    Changed log4j2 config and added logs for utilities

--- src/main/java/com/encryption/tripledes/utils/Utility.java
+++ src/main/java/com/encryption/tripledes/utils/Utility.java
@@ -1,13 +1,20 @@
 package com.encryption.tripledes.utils;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.encryption.tripledes.exceptions.CustomCheckException;
 
 public class Utility {
 	
+	private Logger logger = LogManager.getLogger(Utility.class);
+	
 	public void verifyHeader(String header) throws CustomCheckException {
 		if(header.equals("")) {
+			logger.error("Error {}", "Header is null");
 			throw new CustomCheckException("Header cannot be null");
 		}else if(!header.equalsIgnoreCase("application/x-www-form-urlencoded")) {
+			logger.error("Error {}", "Header has an invalid value");
 			throw new CustomCheckException("Invalid header value");
 		}
 	}
