4d17d13abdd11ba4a3b49d5cf56987c01ba76159
Emilio Gumayagay <emiliogumayagay@gmail.com>
2019-11-20 13:57:54 +0800

    changes to test

--- src/test/java/com/encryption/tripledes/TripleDesEncryptionServiceTest.java
+++ src/test/java/com/encryption/tripledes/TripleDesEncryptionServiceTest.java
@@ -19,33 +19,35 @@ import com.encryption.tripledes.exceptions.CustomCheckException;
 import com.encryption.tripledes.services.TripleDesEncryptionImpl;
 import com.encryption.tripledes.services.TripleDesEncryptionService;
 
+@SpringBootTest(classes={TripleDesEncryptionImpl.class})
 @RunWith(SpringRunner.class)
-public class TripleDesEncryptionServiceTest {	
+public class TripleDesEncryptionServiceTest {
 	@Autowired
-	private TripleDesEncryptionService tripleDesEncryptService = new TripleDesEncryptionImpl();
-	
+	private TripleDesEncryptionService tripleDesEncryptService;
+
+	private CommonRequestDTO commonRequestDTO;
+
+	private AccountRequestDTO accountRequestDTO;
+
+
 	@Before
-	public AccountRequestDTO accountRequestObject () {
-		return new AccountRequestDTO("Password123", "1223123123", "tag-7899", (Double) 4500.00, "smart", "12312312414");
+	public void setUp(){
+		ReflectionTestUtils.setField(tripleDesEncryptService, "secretKey", "Password123");
+
+		commonRequestDTO = new  CommonRequestDTO("test the string in here 1234566 additional data");
+
+		accountRequestDTO =  new AccountRequestDTO("Password123", "1223123123", "tag-7899", (Double) 4500.00, "smart", "12312312414");
 	}
-	
-	@Before
-	public CommonRequestDTO stringRequest () {
-		return new CommonRequestDTO("test the string in here 1234566 additional data");
-	}
-	
+
 	@Test
 	public void testCipherObject () throws CustomCheckException {
-		AccountRequestDTO request = accountRequestObject();
-		ReflectionTestUtils.setField(tripleDesEncryptService, "secretKey", "Password123");
-		ResponseEntity<CommonResponseDTO> response = tripleDesEncryptService.encrypt(request);
+		ResponseEntity<CommonResponseDTO> response = tripleDesEncryptService.encrypt(accountRequestDTO);
 		ResponseEntity<AccountResponseDTO> acctResponse = tripleDesEncryptService.decryptObject(response.getBody().getResponseData());
 		assertThat(accountRequestObject()).isEqualToComparingFieldByField(acctResponse.getBody());
 	}
-	
+
 	@Test
 	public void testCipherString () throws CustomCheckException {
-		ReflectionTestUtils.setField(tripleDesEncryptService, "secretKey", "Password123");
 		ResponseEntity<CommonResponseDTO> response = tripleDesEncryptService.encrypt(stringRequest().getData());
 		ResponseEntity<CommonResponseDTO> decryptResponse= tripleDesEncryptService.decrypt(response.getBody().getResponseData());
 		assertEquals(stringRequest().getData(), decryptResponse.getBody().getResponseData());
